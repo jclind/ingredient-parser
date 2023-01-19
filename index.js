ea6501f100ef6d792fe55d147022f55d523c634c
Jesse Lind <Jesseclind@gmail.com>
2023-01-19 16:18:53 -0500

    add spoonacular api key as a parameter to ingredientParser

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 const { parse } = require('recipe-ingredient-parser-v3')
 
-const ingredientParser = async ingrString => {
+const ingredientParser = async (ingrString, spoonacularAPIKey) => {
   const parsedIngr = parse(ingrString, 'eng')
   const formattedIngr = parsedIngr.ingredient
     .trim()
@@ -13,25 +13,23 @@ const ingredientParser = async ingrString => {
     .replace(/^(fl )/, '')
     .replace(/^(oz )/, '')
     .trim()
-  console.log(formattedIngr)
-  console.log(parsedIngr)
   if (formattedIngr) {
-    const ingrData = await getIngredientInfo(formattedIngr)
+    const ingrData = await getIngredientInfo(formattedIngr, spoonacularAPIKey)
     return ingrData
   } else {
     return null
   }
 }
 
-async function getIngredientInfo(ingrName) {
+async function getIngredientInfo(ingrName, spoonacularAPIKey) {
   const ingrNameLower = ingrName.toLowerCase()
-  console.log(ingrNameLower)
   if (!ingrNameLower) return
   const mongoIngrData = await checkIngredient(ingrNameLower)
-  console.log(mongoIngrData)
   if (!mongoIngrData.data) {
-    const ingrData = await getSpoonacularIngrData(ingrNameLower)
-    console.log(ingrData, 'test')
+    const ingrData = await getSpoonacularIngrData(
+      ingrNameLower,
+      spoonacularAPIKey
+    )
     return ingrData
   } else {
     return mongoIngrData.data
@@ -41,29 +39,44 @@ async function getIngredientInfo(ingrName) {
 async function checkIngredient(name) {
   return await mongoHttp.get(`checkIngredient?name=${name}`)
 }
-async function getSpoonacularIngrData(name) {
-  // console.log(process.env.SPOONACULAR_API_KEY)
-  const searchedIngr = await spoonacularHttp.get(
-    `search?query=${name}&number=1&apiKey=${process.env.SPOONACULAR_API_KEY}`
-  )
-  console.log(searchedIngr, 'test2')
+async function getSpoonacularIngrData(name, spoonacularAPIKey) {
+  let searchedIngr = null
+  try {
+    searchedIngr = await spoonacularHttp.get(
+      `search?query=${name}&number=1&apiKey=${spoonacularAPIKey}`
+    )
+  } catch (error) {
+    const res = error.response.data
+    if (res.code === 401) {
+      return { errorMsg: 'API Key Not Valid', error }
+    } else {
+      return { errorMsg: 'Error Occurred, Please Try Again', error }
+    }
+  }
 
   if (
     !searchedIngr.data ||
     !searchedIngr.data.results ||
     searchedIngr.data.results.length <= 0
   ) {
-    // console.log('no searchedIngr data')
     return null
   }
 
   const ingrId = searchedIngr.data.results[0].id
 
-  const ingrData = await spoonacularHttp.get(
-    `${ingrId}/information?amount=1&apiKey=${process.env.SPOONACULAR_API_KEY}`
-  )
+  let ingrData = null
+  try {
+    ingrData = await spoonacularHttp.get(
+      `${ingrId}/information?amount=1&apiKey=${process.env.SPOONACULAR_API_KEY}`
+    )
+  } catch (error) {
+    if (res.code === 401) {
+      return { errorMsg: 'API Key Not Valid', error }
+    } else {
+      return { errorMsg: 'Error Occurred, Please Try Again', error }
+    }
+  }
   const mongoDBIngrData = { ...ingrData.data, name }
-  // console.log(mongoDBIngrData)
   setMongoDBIngrData(mongoDBIngrData)
   return mongoDBIngrData
 }
