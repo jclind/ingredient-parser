58a0c5ef1ed04f56d075407b6755d46bbad159a3
Jesse Lind <Jesseclind@gmail.com>
2023-03-06 20:25:23 -0500

    test: add test export to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-export * from './funcs/ingredientParser'
+import ingredientParser from './funcs/ingredientParser'
+
+export { ingredientParser }
+const test = () => console.log(test)
+export { test }
+
 export * from '@jclind/ingredient-parser'
